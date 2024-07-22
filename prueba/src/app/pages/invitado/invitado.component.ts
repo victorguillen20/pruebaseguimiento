b8fb9c3c1b77194990f4f0f197daf42a72d55b09
victorguillen20 <57498987+victorguillen20@users.noreply.github.com>
2024-07-22 09:14:43 -0500

    Generando pagina de invitados

---
+++ prueba/src/app/pages/invitado/invitado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invitado',
+  standalone: true,
+  imports: [],
+  templateUrl: './invitado.component.html',
+  styleUrl: './invitado.component.css'
+})
+export class InvitadoComponent {
+
+}
